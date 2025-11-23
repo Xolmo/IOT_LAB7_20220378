d166a3cda6f59c2f8f70d41073da982f35c67aaf
Xolmo <a20220378@pucp.edu.pe>
2025-11-22 23:52:08 -0500

    validacion

---
+++ validacion-service/src/main/java/com/example/validacionservice/ValidacionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.validacionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidacionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidacionServiceApplication.class, args);
+    }
+
+}
